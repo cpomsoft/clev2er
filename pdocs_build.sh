46dc84dfbf0ef751fd763b5f777099a6fa46f209
Alan Muir <a.muir@ucl.ac.uk>
2023-06-09 18:27:56 +0100

    Completed CLEV2ER pre-commit static code tests

--- pdocs_build.sh
+++ pdocs_build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-pdoc -o docs src/clev2er  --logo "http://www.cpom.ucl.ac.uk/downloads/mssl_logo.png" --docformat google
+pdoc -o docs src/clev2er  --logo "https://tinyurl.com/3prhssev" --docformat google
+
+#pdoc -o docs src/clev2er  --logo "http://www.cpom.ucl.ac.uk/downloads/mssl_logo.png" --docformat google
 git add docs
 
 exit 0
