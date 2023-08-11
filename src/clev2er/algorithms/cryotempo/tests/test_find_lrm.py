dcdfcbbfc36aa7ec75e5002d16fbb8aaa40eb305
Alan Muir <a.muir@ucl.ac.uk>
2023-08-11 17:38:53 +0100

    Completed CLEV2ER pre-commit static code tests

--- src/clev2er/algorithms/cryotempo/tests/test_find_lrm.py
+++ src/clev2er/algorithms/cryotempo/tests/test_find_lrm.py
@@ -1,5 +1,7 @@
 """ test of find_lrm.py
 """
+from os import environ
+
 import pytest
 
 from clev2er.algorithms.cryotempo.find_lrm import FileFinder
@@ -9,14 +11,16 @@ def test_find_lrm():
     """test the clev2er.algorithms.cryotempo.find_lrm.FileFinder class"""
     finder = FileFinder()
 
+    assert "CPDATA_DIR" in environ, "Missing env variable CPDATA_DIR"
+
     # finder.add_month(1)
     finder.add_year(2020)
     finder.add_year(2019)
     finder.set_baselines("DE")
-    finder.set_base_path("/raid6/cpdata/SATS/RA/CRY/L1B/LRM/2019/05")
+    finder.set_base_path(f'{environ["CPDATA_DIR"]}/SATS/RA/CRY/L1B/LRM/2019/05')
     files = finder.find_files(flat_search=True)
     assert len(files) > 0, "Should find some files"
-    finder.set_base_path("/raid6/cpdata/SATS/RA/CRY/L1B")
+    finder.set_base_path(f'{environ["CPDATA_DIR"]}/SATS/RA/CRY/L1B')
     files = finder.find_files()
     assert len(files) > 0, "Should find some files"
     with pytest.raises(FileNotFoundError):
