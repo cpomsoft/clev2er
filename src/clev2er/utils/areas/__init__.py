9a48f6e93242cf73a98abe3eb017c81338e8d2bd
Alan Muir <a.muir@ucl.ac.uk>
2024-01-24 18:40:39 +0000

    added documentation for areas

--- src/clev2er/utils/areas/__init__.py
+++ src/clev2er/utils/areas/__init__.py
@@ -20,17 +20,17 @@ The area naming convention used is as follows:
 
 **area**&#95;*background*&#95;*datamask*.py  where *background* and *datamask* are optional.
 
-| Area Name| Background | Data Mask |
+| area_name | Background | Mask |
 |--|--|--|
-| antarctica |  basic land	| None |
-| antarctica_is | basic land | grounded ice sheet : bedmachine mask|
-| antarctica_fi | basic land | floating ice only : bedmachine mask|
-| antarctica_hs_is | hill shade | grounded ice sheet : bedmachine mask|
-| antarctica_hs_fi | hill shade | floating ice only : bedmachine mask|
-| greenland |  basic land	| None |
-| greenland_is | basic land | grounded ice sheet : bedmachine mask|
-| greenland_fi | basic land | floating ice only : bedmachine mask|
-| greenland_hs_is | hill shade | grounded ice sheet : bedmachine mask|
-| greenland_hs_fi | hill shade | floating ice only : bedmachine mask|
+| antarctica |  basic_land	| None |
+| antarctica_is | basic_land | antarctica_bedmachine_v2_grid_mask[2,4] == grounded ice sheet |
+| antarctica_fi | basic_land | floating ice only : bedmachine mask|
+| antarctica_hs_is | hillshade | antarctica_bedmachine_v2_grid_mask[2,4] == grounded ice sheet |
+| antarctica_hs_fi | hillshade | floating ice only : bedmachine mask|
+| greenland |  basic_land	| None |
+| greenland_is | basic_land | antarctica_bedmachine_v2_grid_mask[2,4] == grounded ice sheet |
+| greenland_fi | basic_land | floating ice only : bedmachine mask|
+| greenland_hs_is | hillshade | antarctica_bedmachine_v2_grid_mask[2,4] == grounded ice sheet |
+| greenland_hs_fi | hillshade | floating ice only : bedmachine mask|
 
 """
